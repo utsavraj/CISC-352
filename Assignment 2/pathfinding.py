817f00349750634b34edad350c2ba310b0b80021
Utsav Raj <49531059+utsavraj@users.noreply.github.com>
2020-03-02 16:40:00 -0500

    Update pathfinding.py

--- Assignment 2/pathfinding.py
+++ Assignment 2/pathfinding.py
@@ -1,6 +1,7 @@
-# pathfinding_a.txt
+## INPUT: pathfinding_a.txt
+# pathfinding_a_out.txt
 ## Up, Right, Left, Down for 1 - Greedy and A*
 
-
+## INPUT: pathfinding_b.txt
 # pathfinding_b_out.txt
 ## Up, Right, Left, Down, Diagonal for 1 - Greedy and A*
